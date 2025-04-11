d29a44701a612380c16ff2b7c9079e0f009e1b00
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-04-10 21:48:01 -0400

    init commit

---
+++ src/test/java/com/example/CarRentalService_DbFinalProject/CarRentalServiceDbFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarRentalService_DbFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalServiceDbFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
