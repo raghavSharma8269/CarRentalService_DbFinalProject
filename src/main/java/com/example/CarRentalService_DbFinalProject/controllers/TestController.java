136ab805cc7c022eb30a1e4bedcffebed74f66b2
Ammar Khalil <ammarkhalil786@gmail.com>
2025-04-22 12:47:33 -0400

    Adjustments Needed, Review

--- src/main/java/com/example/CarRentalService_DbFinalProject/controllers/TestController.java
+++ src/main/java/com/example/CarRentalService_DbFinalProject/controllers/TestController.java
@@ -1,5 +1,6 @@
 package com.example.CarRentalService_DbFinalProject.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 
     @GetMapping("/employee-admin-dash")
+    @PreAuthorize("hasRole('EMPLOYEE') or hasRole('ADMIN')")
     public String employeeAdminDash() {
         return "employee-admin-dash"; // returns employee-admin-dash.html
     }
