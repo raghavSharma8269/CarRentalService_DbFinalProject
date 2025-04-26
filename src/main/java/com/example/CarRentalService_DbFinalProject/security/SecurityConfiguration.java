4ae50cda1c046d40ee27d4222a7de8858fa0748f
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-04-25 22:11:56 -0400

    small fix

--- src/main/java/com/example/CarRentalService_DbFinalProject/security/SecurityConfiguration.java
+++ src/main/java/com/example/CarRentalService_DbFinalProject/security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -45,7 +46,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         // allow anonymous access to login page & static resources
                         .requestMatchers("/login", "/css/**", "/js/**").permitAll()
